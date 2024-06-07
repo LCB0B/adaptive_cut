4d1d650550a5e8ceca595c2c7ae90ad1a1d4fc9a
Louis_B <louisboucherie@protonmail.com>
2024-06-07 17:06:03 +0200

    exemple

--- AdaptiveCut/LinkClustering.py
+++ AdaptiveCut/LinkClustering.py
@@ -10,44 +10,11 @@ import sys
 import time 
 from tqdm import tqdm
 
+
+from .utils import *
+
 sys.setrecursionlimit(int(1e4))
 # Mapping from (i, j) in adjacency matrix to index in condensed distance matrix
-
-def index_2to1(i, j, n):
-    if i > j:
-        i, j = j, i
-    return n * i + j - ((i + 2) * (i + 1)) // 2
-
-def index_1to2(k, n):
-    i = n - 2 - int(math.sqrt(-8 * k + 4 * n * (n - 1) - 7) / 2.0 - 0.5)
-    j = (k + (i + 2) * (i + 1) // 2)%n
-    return i, j
-
-def Dc(m, n):
-    try:
-        return (m * (m - n + 1.0)) / ((n - 2.0) * (n - 1.0))
-    except ZeroDivisionError:
-        return 0.0
-
-
-def Dc2(m, n):
-    try:
-        return ( (m - n + 1.0)) / (n*(n - 1.0)/2 -(n - 1.0))
-    except ZeroDivisionError:
-        return 0.0
-
-def compute_partition_density(edges2com,inv_edges):
-    ms = [len(e) for e in edges2com.values()]
-    list_edges = [e for e in edges2com.values()]
-    list_nodes = [ set([ node for edges in e for node in inv_edges[edges] ]) for e in list_edges ]
-    ns = [len(n) for n in list_nodes]
-    D = 0
-    for n,m in zip(ns,ms):
-        D += m*Dc2(m,n)
-    return D / np.sum(ms)
-
-def entropy(comm):
-    return -np.sum([c*np.log2(c) for c in comm if c > 0])
     
 class LinkClustering:
     def __init__(self, name, delimiter,file='data'):
@@ -640,9 +607,13 @@ class LinkClustering:
         #for each node get % communities it belongs to, node_appartenence = {node: {community_id: %}}
         node_appartenence = defaultdict(dict)
         for com_id,edges in partition_edges.items():
-            for e in edges:
-                for node in e:
-                    node_appartenence[node][com_id] += 1/len(self.adj[node])
+            for edge in edges:
+                for node in edge:
+                    if com_id in node_appartenence[node]:
+                        node_appartenence[node][com_id] += 1/len(self.adj[node])
+                    else:
+                        node_appartenence[node][com_id] = 1/len(self.adj[node])
+        return node_appartenence
         
     
     def build_dendrogram(self):
